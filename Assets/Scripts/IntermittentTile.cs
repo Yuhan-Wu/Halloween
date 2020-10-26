f379f6fd17169443b60fc9e1ac4eaa914cb7132c
Yuhan Wu <iriswuyuhan@gmail.com>
2020-10-25 23:03:31 -0600

    Add fire and spike

---
+++ Assets/Scripts/IntermittentTile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntermittentTile : MonoBehaviour
+{
+    public float Appear = 0;
+    public float Disappear = 0;
+
+    private bool Activated = true;
+
+    private bool isCoroutineExecuting = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Activate());
+    }
+
+    IEnumerator Activate()
+    {
+        if (isCoroutineExecuting)
+            yield break;
+
+        isCoroutineExecuting = true;
+        float waitTime = Activated ? Appear : Disappear;
+        yield return new WaitForSeconds(waitTime);
+
+        Activated = !Activated;
+        GetComponent<Collider>().enabled = Activated;
+        GetComponent<MeshRenderer>().enabled = Activated;
+        isCoroutineExecuting = false;
+        
+    }
+}
