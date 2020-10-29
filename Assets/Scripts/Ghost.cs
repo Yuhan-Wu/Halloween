811d738c6d3d2cb8a98de883769376a5d10cda38
Jieyina <nasjieyi@gmail.com>
2020-10-29 17:49:32 +0800

    fix ghost reference lost

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class Ghost : Monster
 {
-    private static bool init = false;
-    private static HashSet<GameObject> emptyMirrors;
-    [SerializeField]
     private GameObject curMirror;
     private GameObject prevMirror;
     [SerializeField]
     private float moveSpeed = 2f;
 
-    private void Awake()
-    {
-        if (!init)
-        {
-            emptyMirrors = new HashSet<GameObject>(GameObject.FindGameObjectsWithTag("Mirror"));
-            init = true;
-        }      
-    }
-
     protected override void Start()
     {
         base.Start();
         float minDis = float.MaxValue;
-        foreach (GameObject mirror in emptyMirrors)
+        foreach (GameObject mirror in LevelManager.Instance.EmptyMirrors)
         {
             float dis = Vector3.Distance(transform.position, mirror.transform.position);
             if (dis < minDis)
@@ -33,7 +21,7 @@ public class Ghost : Monster
         }
         transform.position = curMirror.transform.position;
         transform.rotation = curMirror.transform.rotation;
-        emptyMirrors.Remove(curMirror);
+        LevelManager.Instance.EmptyMirrors.Remove(curMirror);
     }
 
     // Update is called once per frame
@@ -45,7 +33,7 @@ public class Ghost : Monster
                 currentState = EnemyStates.Idle;
             prevMirror = curMirror;
             float minDis = Vector3.Distance(objectToChase.position, curMirror.transform.position);
-            foreach (GameObject mirror in emptyMirrors)
+            foreach (GameObject mirror in LevelManager.Instance.EmptyMirrors)
             {
                 float dis = Vector3.Distance(objectToChase.position, mirror.transform.position);
                 if (dis < minDis)
@@ -53,8 +41,8 @@ public class Ghost : Monster
             }
             if (curMirror != prevMirror)
             {
-                emptyMirrors.Add(prevMirror);
-                emptyMirrors.Remove(curMirror);
+                LevelManager.Instance.EmptyMirrors.Add(prevMirror);
+                LevelManager.Instance.EmptyMirrors.Remove(curMirror);
             }
             transform.position = curMirror.transform.position;
             transform.rotation = curMirror.transform.rotation;
@@ -71,11 +59,5 @@ public class Ghost : Monster
             transform.rotation  = Quaternion.Euler(new Vector3(0, rotationAngle.y, 0));
             transform.Translate(moveSpeed * direction.normalized * Time.deltaTime, Space.World);
         }
-    }
-
-    protected override void OnCollisionEnter(Collision collision)
-    {
-        base.OnCollisionEnter(collision);
-        init = false;
     }
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance = null;
+
+    private HashSet<GameObject> emptyMirrors;
+    public HashSet<GameObject> EmptyMirrors => emptyMirrors;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        emptyMirrors = new HashSet<GameObject>(GameObject.FindGameObjectsWithTag("Mirror"));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
